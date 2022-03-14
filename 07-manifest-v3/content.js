c2351ed4116384117427f2b2c92be97db1208342
ahpkadota <58079967+ahpkadota@users.noreply.github.com>
2022-03-14 10:00:28 +0000

    Delete 07-manifest-v3 directory

--- 07-manifest-v3/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        var asdfg = document.getElementsByClassName("semi-descriptions-value");
-        if (asdfg.length > 0)
-            sendResponse({farewell: Array.prototype.map.call(asdfg, function(x){return x.innerHTML}).join("")});
-});
