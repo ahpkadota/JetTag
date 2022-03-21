596c70555ce699caddd1ba9297f6e8f5b63a6a22
ahpkadota <58079967+ahpkadota@users.noreply.github.com>
2022-03-21 09:21:42 +0000

    Update content.js

--- JetTag 1.0.0/content.js
+++ JetTag 1.0.0/content.js
@@ -1,6 +1,19 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        var asdfg = document.getElementsByClassName("semi-descriptions-value");
-        if (asdfg.length > 0)
-            sendResponse({farewell: Array.prototype.map.call(asdfg, function(x){return x.innerHTML}).join("")});
+        let a = document.getElementsByClassName("semi-descriptions-key");
+        let b = [];
+        let c = document.getElementsByClassName("semi-tag-content");
+        let d = [];
+        for (let i=0;i<a.length;i++) {
+            if (a[i].firstChild.innerHTML == "selectedPolicyCode") {
+                b.push(a[i].parentElement.nextSibling.firstChild.innerHTML);
+            }
+        }
+        for (let j=0;j<c.length;j++) {
+            d.push(c[j].innerText)
+        }
+        if (b.length > 0)
+            sendResponse({farewell: b.map(x=>x.split(",")).flat(), fair: d.join("")});
 });
+
+
