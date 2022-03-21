26bf7b0143487f8773d67302ccae9b537778c17d
ahpkadota <58079967+ahpkadota@users.noreply.github.com>
2022-03-21 09:19:04 +0000

    Update popup.js

--- JetTag 1.0.0/popup.js
+++ JetTag 1.0.0/popup.js
@@ -8,18 +8,25 @@ $(document).ready(
             chrome.tabs.sendMessage(tabs[0].id, {
                 greeting: "hello"
             }, function(response) {
-                let titles = data.map(x => x.children.map(y => y.text)).flat();
+                let titles = data.slice(0,data.length-2).map(x => x.children.map(y => y.id)).flat();
+                let cases = [data[data.length-1]].map(x => x.children.map(y => y.id)).flat();
                 let idx = [];
-                for (let i = 0; i < titles.length; i++) {
-                    var replace = "," + titles[i].replace(/[()&]/g, "") + ";";
-                    var re = new RegExp(replace, "g");
-                    if ((response.farewell.replace(/[()&]/g, "").match(re) || []).length == 1) {
-                        idx.push(i)
+                for (let i = 0; i < cases.length; i++) {
+                    if (response.fair.includes(cases[i])) {
+                        let but = document.getElementById("case");
+                        but.style = "display: block";
+                        but.innerHTML = cases[i];
+                        but.addEventListener("click", function(){$('#preview').val(data[data.length-1].children[i].text + " \nREASON: \n" + data[data.length-1].children[i].ft);})
                     }
                 }
-                $('#multipleSelectExample').val(idx);
+                for (let i = 0; i < response.farewell.length; i++) {
+                    if (titles.includes(response.farewell[i])) {
+                        idx.push(response.farewell[i])
+                    }
+                }
+                $('#multipleSelectExample').val(idx.filter(item => idx.lastIndexOf(item) == idx.indexOf(item)));
                 const sel = $('#multipleSelectExample').select2('data');
-                $('#preview').val(sel.map(x => x.fullText).join("\r\n")+ "\nREASON: ");
+                $('#preview').val(sel.map(x => x.ft).join("\r\n")+ "\nREASON: \nHello, . Thank you.");
                 $('#multipleSelectExample').trigger('change');
             });
         });
@@ -37,8 +44,42 @@ $(document).ready(
 
         //gets options from data variable
         $('#multipleSelectExample').select2({
-            data: data
+            data: data.slice(0,data.length-1)
+        }).trigger('change')
+        let check = document.getElementById("flexCheckDefault");
+        check.addEventListener("change", function() {
+            if (document.getElementById("flexCheckDefault").checked) {
+                $('#multipleSelectExample').select2('destroy').empty()
+                $('#multipleSelectExample').attr("multiple", false)
+                $('#multipleSelectExample').select2({
+                    data: [data[data.length-1]]
+                }).trigger('change')
+                document.getElementById("preview").value = ""
+                $('#multipleSelectExample').on('select2:select select2:unselect', function(e) {
+                    const sel = $('#multipleSelectExample').select2('data')
+                    document.getElementById("preview").value = sel.map(x => x.text) + "\nREASON: \n" + sel.map(x => x.ft).join("\r\n")
+                    if (sel == "") {
+                        document.getElementById("preview").value = ""
+                    }
+                });
+            } else {
+                $('#multipleSelectExample').select2('destroy').empty()
+                $('#multipleSelectExample').attr("multiple", true)
+                $('#multipleSelectExample').select2({
+                    data: data.slice(0,data.length-1)
+                }).trigger('change')
+                document.getElementById("preview").value = ""
+                $('#multipleSelectExample').on('select2:select select2:unselect', function(e) {
+                    const sel = $('#multipleSelectExample').select2('data')
+                    document.getElementById("preview").value = sel.map(x => x.ft).join("\r\n") + "\nREASON: Hello, . Thank you."
+                    if (sel == "") {
+                        document.getElementById("preview").value = ""
+                    }
+                });
+            }
         })
+        
+        
         // Set the value, creating a new option if necessary
         if ($('#multipleSelectExample').find("option[value='" + data.id + "']").length) {
             $('#multipleSelectExample').val(data.id).trigger('change');
@@ -58,7 +99,7 @@ $(document).ready(
         //sets selected tag values to textarea element for preview
         $('#multipleSelectExample').on('select2:select select2:unselect', function(e) {
             const sel = $('#multipleSelectExample').select2('data')
-            document.getElementById("preview").value = sel.map(x => x.fullText).join("\r\n") + "\nREASON: "
+            document.getElementById("preview").value = sel.map(x => x.ft).join("\r\n") + "\nREASON: Hello, . Thank you."
             if (sel == "") {
                 document.getElementById("preview").value = ""
             }
@@ -68,9 +109,12 @@ $(document).ready(
         document.getElementById("save").addEventListener("click", function() {
             try {
                 let value = document.getElementById("input").value;
-                JSON.parse(value);
+                var jsonStr = value.replace(/(\w+:)|(\w+ :)/g, function(s) {
+                    return '"' + s.substring(0, s.length-1) + '":';
+                  });
+                JSON.parse(jsonStr);
                 chrome.storage.local.set({
-                    key: value
+                    key: jsonStr
                 }, toast("Success!"))
             } catch (err) {
                 toast("Invalid format! :(")
@@ -85,6 +129,9 @@ let data = [];
 chrome.storage.local.get(['key'], function(result) {
     data = JSON.parse(result.key);
 });
+
+
+  
 
 function toast(x) {
     var toastLiveExample = document.getElementById('liveToast');
