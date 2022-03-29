6a1a2f157c8d66fbbe6e2c157d8baadcc796ae06
ahpkadota <58079967+ahpkadota@users.noreply.github.com>
2022-03-29 12:27:18 +0100

    1.0.3

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+        if (request.greeting === "hello") {
         let a = document.getElementsByClassName("semi-descriptions-key");
         let b = [];
         let c = document.getElementsByClassName("semi-tag-content");
@@ -14,6 +15,56 @@ chrome.runtime.onMessage.addListener(
         }
         if (b.length > 0)
             sendResponse({farewell: b.map(x=>x.split(",")).flat(), fair: d.join("")});
+    }
+});
+
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.tabTitle > 0) {
+        chrome.storage.local.get(['lol'], function(result) {
+            let val = document.activeElement.value
+            let start = document.activeElement.selectionStart;
+            let end = document.activeElement.selectionEnd;
+            let txt = result.lol[request.tabTitle-1][1];
+            document.activeElement.value = val.substr(0,start) + txt + val.substr(end,val.length);
+            document.activeElement.setSelectionRange(txt.length+start, txt.length+start)
+            document.activeElement.dispatchEvent(new Event('input', {bubbles:true}));
+        });
+    } else {
+        let a = document.getElementsByClassName("semi-descriptions-key");
+        let b = [];
+        for (let i=0;i<a.length;i++) {
+            if (a[i].firstChild.innerHTML == "selectedPolicyCode") {
+                b.push(a[i].parentElement.nextSibling.firstChild.innerHTML);
+            }
+        }
+        let c = b.map(x=>x.split(",")).flat();
+        let d = c.filter(item => c.lastIndexOf(item) == c.indexOf(item))
+        let titles = data.slice(0,data.length-2).map(x => x.children.map(y => y.id)).flat();
+        let titles2 = data.slice(0,data.length-2).map(x => x.children.map(y => y.ft)).flat();
+        let e = [];
+        for (let i=0;i<titles.length;i++) {
+            for (let j=0;j<d.length;j++) {
+                if (d[j] == titles[i]) {
+                    e.push(titles2[i])
+                }
+            }
+        }
+        let val = document.activeElement.value
+        let start = document.activeElement.selectionStart;
+        let end = document.activeElement.selectionEnd;
+        let txt = e.join("\n")+"\nREASON:\n";
+        document.activeElement.value = val.substr(0,start) + txt + val.substr(end,val.length);
+        document.activeElement.setSelectionRange(txt.length+start, txt.length+start)
+        document.activeElement.dispatchEvent(new Event('input', {bubbles:true}));
+    }
+});
+
+
+let data = [];
+chrome.storage.local.get(['key'], function(result) {
+    data = JSON.parse(result.key);
 });
 
 
